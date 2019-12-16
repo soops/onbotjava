0edf1598422d568332ab6ac1663157d5090ea78e
Ethan Arterberry <ethanarterberry@gmail.com>
2019-12-16 13:51:45 -0800

    Update TeleMech.java

--- TeleMech.java
+++ TeleMech.java
@@ -1,6 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -20,8 +25,7 @@ public class TeleMech extends LinearOpMode {
     private DcMotor frontLeft;
     private DcMotor frontRight;
     private DcMotor liftMotor;
-    private Gyroscope imu_1;
-    private Gyroscope imu;
+    BNO055IMU imu;
     private Servo foundationServo;
 
     public void runOpMode() {
@@ -44,17 +48,29 @@ public class TeleMech extends LinearOpMode {
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.mode                = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled      = false;
+        imu.initialize(parameters);
+        
         boolean liftMode = false;
         boolean liftModeChanged = false;
         boolean slow = false;
         boolean slowChanged = false;
+        boolean slow2 = false;
+        boolean slow2Changed = false;
         boolean servoDown = false;
         boolean servoChanged = false;
         
-        double r, theta;
         double powerFL, powerFR, powerBL, powerBR;
         double scale = 1.0;
-    
+        double scale2 = 1.0;
+        
         while (opModeIsActive()) {
             
             if (gamepad1.left_stick_button && !liftModeChanged) {
@@ -64,11 +80,19 @@ public class TeleMech extends LinearOpMode {
                 liftModeChanged = false;
             }
             
+            
             if (gamepad1.a && !slowChanged) {
                 slow = !slow;
                 slowChanged = true;
             } else if (!gamepad1.a) {
                 slowChanged = false;
+            }
+            
+            if (gamepad2.b && !slow2Changed) {
+                slow2 = !slow2;
+                slow2Changed = true;
+            } else if (!gamepad1.b) {
+                slow2Changed = false;
             }
             
             if (gamepad1.left_bumper && !servoChanged) {
@@ -78,23 +102,48 @@ public class TeleMech extends LinearOpMode {
                 servoChanged = false;
             }
             
+            
             if (servoDown) { foundationServo.setPosition(0.5); } else { foundationServo.setPosition(0); }
             
-            if (slow) { scale = 0.5; } else { scale = 1.0; }
+            if (slow)  { scale = 0.5; }   else { scale = 1.0; }
+            if (slow2) { scale2 = 0.5; }  else { scale2 = 1.0; }
             
             if (!liftMode) {
-                r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-                theta = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+                Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
                 
-                powerFL = (r * Math.cos(theta)) - gamepad1.right_stick_x;
-                powerFR = (r * Math.sin(theta)) + gamepad1.right_stick_x;
-                powerBL = (r * Math.sin(theta)) - gamepad1.right_stick_x;
-                powerBR = (r * Math.cos(theta)) + gamepad1.right_stick_x;
+                double gyro_degrees = angles.firstAngle;
+                double gyro_radians = gyro_degrees * Math.PI / 180;
+                double forward = gamepad1.left_stick_y * Math.cos(gyro_radians) + gamepad1.left_stick_x * Math.sin(gyro_radians);
+                double strafe = -gamepad1.left_stick_y * Math.sin(gyro_radians) + gamepad1.left_stick_x * Math.cos(gyro_radians);
                 
-                frontLeft.setPower(powerFL * scale);
-                frontRight.setPower(powerFR * scale);
-                backLeft.setPower(powerBL * scale);
-                backRight.setPower(powerBR * scale);
+                powerFL = forward - strafe - gamepad1.right_stick_x;
+                powerFR = forward + strafe + gamepad1.right_stick_x;
+                powerBL = forward + strafe - gamepad1.right_stick_x;
+                powerBR = forward - strafe + gamepad1.right_stick_x;
+                
+                frontLeft.setPower(Range.clip(powerFL * scale, -1, 1));
+                frontRight.setPower(Range.clip(powerFR * scale, -1, 1));
+                backLeft.setPower(Range.clip(powerBL * scale, -1, 1));
+                backRight.setPower(Range.clip(powerBR * scale, -1, 1));
+                
+                if (gamepad1.right_trigger > 0.0) {
+                    angleMotor.setPower((double) (Range.clip(gamepad1.right_trigger, -1, 1)));
+                } else if (gamepad1.left_trigger > 0.0) {
+                    angleMotor.setPower(-1.0 * (double) (Range.clip(gamepad1.left_trigger, -1, 1)));
+                } else {
+                    angleMotor.setPower(0.0);
+                }
+                
+                if (gamepad1.right_bumper) {
+                    liftMotor.setPower(0.5);
+                } else if (gamepad1.left_bumper) {
+                    liftMotor.setPower(-0.5);
+                } else {
+                    liftMotor.setPower(0.0);
+                }
+                
+                angleMotor.setPower(gamepad2.left_stick_y * scale2);
+                liftMotor.setPower(gamepad2.right_stick_y * scale2);
             } else {
                 angleMotor.setPower(gamepad1.left_stick_y * scale);
                 liftMotor.setPower(gamepad1.right_stick_y * scale);
